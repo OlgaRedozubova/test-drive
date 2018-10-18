829e08d79ea00c482d7b18b9399a8b603177551f
OlgaRedozubova <olgared82@gmail.com>
2018-10-18 14:07:39 +0300

    second

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ session = losswise.Session(tag='my_dilated_convnet', max_iter=10,
                            params={'cnn_size': 20})
 graph = session.graph('loss', kind='min')
 for x in range(10):
-    train_loss = 1. / (0.1 + x + 0.1 * random.random())
+    train_loss = 2. / (0.1 + x + 0.1 * random.random())
     test_loss = 1.5 / (0.1 + x + 0.2 * random.random())
     graph.append(x, {'train_loss': train_loss, 'test_loss': test_loss})
     time.sleep(1.)
